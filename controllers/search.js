796e8fc6af821835803c419da185cff5b21d791e
Nilesh-Aditya <nileshaditya2000@gmail.com>
2024-01-05 22:11:43 +0530

    project complete

--- controllers/search.js
+++ controllers/search.js
@@ -1,5 +1,18 @@
-function SearchNotes (req, res, next) {
+const Note = require("../models/notes")
 
+async function SearchNotes (req, res, next) {
+    try {        
+        let query = req.query.q;
+        let notes = await Note.find({ $text: { $search: query } }).limit(10);
+        if(!notes){
+            return res.status(400).json({ msg: 'Notes Not found' });
+        }
+    
+        return res.status(200).json({ Notes: notes });
+    } 
+    catch (error) {
+        return res.status(500).json({ error });
+    }
 }
 
 module.exports = {
